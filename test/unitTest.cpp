0bafa5ec414978e6a92b2883d24bbabd8a6cb1f5
Shivix <52997365+Shivix@users.noreply.github.com>
2020-07-12 14:25:31 +0100

    Added addition, subtraction tests

--- test/unitTest.cpp
+++ test/unitTest.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-#include <algorithm>
 #include "../MatLib/matrix.hpp"
 #define BOOST_TEST_MODULE MatLib Test
 #include <boost/test/unit_test.hpp>
@@ -25,24 +23,80 @@ BOOST_AUTO_TEST_CASE(multiply_test)
     BOOST_TEST(resultMatrix == expectedMatrix);
 }
 
-BOOST_AUTO_TEST_CASE(determinant_test)
+BOOST_AUTO_TEST_CASE(addition_test)
 {
     MatLib::matrix<int, 3, 3> matrix1 = {
             {{{0, 1, 2},
                      {3, 4, 5},
                      {6, 7, 8}
              }}};
-    MatLib::matrix<int, 2, 2> matrix2 = {
+    MatLib::matrix<int, 3, 3> matrix2 = {
+            {{{0, 1, 2},
+                     {3, 4, 5},
+                     {6, 7, 8}
+             }}};
+    auto resultMatrix12 = matrix1 + matrix2;
+    MatLib::matrix<int, 3, 3> expectedMatrix12 = {
+            {{{0, 2, 4},
+                     {6, 8, 10},
+                     {12, 14, 16}
+             }}};
+    auto resultMatrix1S = matrix1 + 7;
+    MatLib::matrix<int, 3, 3> expectedMatrix1S = {
+            {{{7, 8, 9},
+                     {10, 11, 12},
+                     {13, 14, 15}
+             }}};
+    BOOST_TEST(resultMatrix12 == expectedMatrix12);
+    BOOST_TEST(resultMatrix1S == expectedMatrix1S);
+}
+
+BOOST_AUTO_TEST_CASE(subtraction_test)
+{
+    MatLib::matrix<int, 3, 3> matrix1 = {
+            {{{0, 1, 2},
+                     {3, 4, 5},
+                     {6, 7, 8}
+             }}};
+    MatLib::matrix<int, 3, 3> matrix2 = {
+            {{{0, 1, 2},
+                     {3, 4, 5},
+                     {6, 7, 8}
+             }}};
+    auto resultMatrix12 = matrix1 - matrix2;
+    MatLib::matrix<int, 3, 3> expectedMatrix12 = {
+            {{{0, 0, 0},
+                     {0, 0, 0},
+                     {0, 0, 0}
+             }}};
+    auto resultMatrix1S = matrix1 - 5;
+    MatLib::matrix<int, 3, 3> expectedMatrix1S = {
+            {{{-5, -4, -3},
+                     {-2, -1, 0},
+                     {1, 2, 3}
+             }}};
+    BOOST_TEST(resultMatrix12 == expectedMatrix12);
+    BOOST_TEST(resultMatrix1S == expectedMatrix1S);
+}
+
+BOOST_AUTO_TEST_CASE(determinant_test)
+{
+    MatLib::matrix<int, 3, 3> matrix3x3 = {
+            {{{0, 1, 2},
+                     {3, 4, 5},
+                     {6, 7, 8}
+             }}};
+    MatLib::matrix<int, 2, 2> matrix2x2 = {
             {{{0, 1},
                      {3, 4}
              }}};
-    MatLib::matrix<int, 4, 4> matrix3 = {
+    MatLib::matrix<int, 4, 4> matrix4x4 = {
             {{{5, 8, 2, 3},
                      {4, 5, 12, 7},
                      {4, 9, 10, 11},
                      {12, 77, 14, 14}
              }}};
-    BOOST_TEST(matrix2.getDeterminant() == -3);
-    BOOST_TEST(matrix1.getDeterminant() == 0);
-    BOOST_TEST(matrix3.getDeterminant() == 14466);
+    BOOST_TEST(matrix2x2.getDeterminant() == -3);
+    BOOST_TEST(matrix3x3.getDeterminant() == 0);
+    BOOST_TEST(matrix4x4.getDeterminant() == 14466);
 }
