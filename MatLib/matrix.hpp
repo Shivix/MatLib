48d666dc7819b06ff73f4d31c29f62242b2fa603
Shivix <52997365+Shivix@users.noreply.github.com>
2020-07-12 14:27:36 +0100

    getDeterminant can now solve up to 3x3

--- MatLib/matrix.hpp
+++ MatLib/matrix.hpp
@@ -3,27 +3,48 @@
 
 #include <cstddef>
 #include <array>
+#include <cmath>
 
 namespace MatLib{
-    template<typename T, std::size_t rows, std::size_t cols>
+    template<typename T, std::size_t cols, std::size_t rows>
     class matrix{
     public:
-        std::array<std::array<T, rows>, cols> m_data{};
+        // No explicit constructor/ destructor etc. for aggregate
+        
+        std::array<std::array<T, cols>, rows> m_data{};
         
         void fill(const T& value){
             std::fill(m_data[0][0], m_data[rows][cols], value);
         }
+        
         T getDeterminant(){
-            return 0;
+            static_assert(rows == cols, "Must be a square matrix");
+            T determinant = 0;
+            if(rows == 1){
+                return m_data[0][0];
+            }
+            else if(rows == 2){
+                return (m_data[0][0] * m_data[1][1]) - (m_data[1][0] * m_data[0][1]);
+            }
+            else if(rows == 3){
+                return get3x3Determinant(0, 0);
+            }
+            else{
+                // TODO: add recursion top solve for matrices of all sizes
+            }
+            return determinant;
+        }
+        std::size_t size() const noexcept{
+            return rows * cols;
         }
         void swap(matrix& other){
             for(size_t i = 0; i < m_data.size(); ++i){
                 m_data[i].swap(other.m_data[i]);
             }
         }
-        
+        // Operators
         matrix operator + (const matrix& other) noexcept { 
-            matrix<T, rows, cols> resultMatrix = {};
+            matrix<T, cols, rows> resultMatrix = {};
             for(std::size_t i = 0; i < rows; ++i){
                 for(std::size_t j = 0; j < cols; ++j){ // m_data[x] will all have same .size()
                     resultMatrix[i][j] = this->m_data[i][j] + other.m_data[i][j];
@@ -32,16 +53,16 @@ namespace MatLib{
             return resultMatrix;
         }
         matrix operator + (const T& scalar) noexcept {
-            matrix<T, rows, cols> resultMatrix = {};
+            matrix<T, cols, rows> resultMatrix = {};
             for(std::size_t i = 0; i < rows; ++i){
-                for(std::size_t j = 0; j < cols; ++j){ // m_data[x] will all have same .size()
+                for(std::size_t j = 0; j < cols; ++j){ 
                     resultMatrix[i][j] = this->m_data[i][j] + scalar;
                 }
             }
             return resultMatrix;
         }
         matrix operator - (const matrix& other) noexcept {
-            matrix<T, rows, cols> resultMatrix = {};
+            matrix<T, cols, rows> resultMatrix = {};
             for(std::size_t i = 0; i < rows; ++i){
                 for(std::size_t j = 0; j < cols; ++j){ // m_data[x] will all have same .size()
                     resultMatrix[i][j] = this->m_data[i][j] - other.m_data[i][j];
@@ -50,18 +71,17 @@ namespace MatLib{
             return resultMatrix;
         }
         matrix operator - (const T& scalar) noexcept {
-            matrix<T, rows, cols> resultMatrix = {};
+            matrix<T, cols, rows> resultMatrix = {};
             for(std::size_t i = 0; i < rows; ++i){
-                for(std::size_t j = 0; j < cols; ++j){ // m_data[x] will all have same .size()
+                for(std::size_t j = 0; j < cols; ++j){ 
                     resultMatrix[i][j] = this->m_data[i][j] - scalar;
                 }
             }
             return resultMatrix;
         }
-
         template<std::size_t otherRows, std::size_t otherCols>
-        matrix operator * (const matrix<T, otherRows, otherCols>& other) noexcept { //array swap may help
-            matrix<T, otherRows, cols> resultMatrix = {};
+        matrix operator * (const matrix<T, otherCols, otherRows>& other) noexcept { 
+            matrix<T, cols, otherRows> resultMatrix = {};
             for(size_t i = 0; i < rows; ++i){
                 for(size_t j = 0; j < cols; ++j){
                     resultMatrix.m_data[i][j] = m_data[i][j] * other.m_data[j][i];
@@ -69,7 +89,7 @@ namespace MatLib{
             }
             return resultMatrix;
         }
-        matrix operator * (const T& scalar) noexcept { //array swap may help
+        matrix operator * (const T& scalar) noexcept {
             for(size_t i = 0; i < rows; ++i){
                 for(size_t j = 0; j < cols; ++j){
                     m_data[i][j] * scalar;
@@ -88,10 +108,10 @@ namespace MatLib{
                 }
             }
         }
-        std::array<T, rows> operator [] (const std::size_t& index) noexcept { // static_assert for boundaries
+        std::array<T, cols> operator [] (const std::size_t& index) noexcept { // static_assert for boundaries
             return m_data[index];
         }
-        std::array<T, rows> operator [] (const std::size_t& index) const noexcept { // static_assert for boundaries
+        std::array<T, cols> operator [] (const std::size_t& index) const noexcept {
             return m_data[index];
         }
         bool operator == (const matrix& other) noexcept {
@@ -118,21 +138,7 @@ namespace MatLib{
             }
             return false;
         }
-        
-        std::size_t size(){
-            std::size_t iter = 0;
-            for(auto&&i: m_data){
-                iter += i.size();
-            }
-            return iter;
-        }
-        std::size_t size() const {
-            std::size_t iter = 0;
-            for(auto&&i: m_data){
-                iter += i.size();
-            }
-            return iter;
-        }
+        // Iterators
         T back() noexcept {
             return *end();
         }
@@ -140,19 +146,19 @@ namespace MatLib{
             return *begin();
         }
         T* begin() noexcept {
-            return static_cast<T*>(&m_data[0][0]);
+            return &m_data[0][0];
         }
         const T* begin() const noexcept {
-            return static_cast<T*>(&m_data[0][0]);
+            return &m_data[0][0];
         }
         const T* cbegin() const noexcept {
             return static_cast<const T*>(&m_data[0][0]);
         }
         T* end() noexcept {
-            return static_cast<T*>(&m_data[rows][cols]);
+            return &m_data[rows][cols];
         }
         const T* end() const noexcept {
-            return static_cast<T*>(&m_data[rows][cols]);
+            return &m_data[rows][cols];
         }
         const T* cend() const noexcept {
             return static_cast<const T*>(&m_data[rows][cols]);
@@ -163,8 +169,13 @@ namespace MatLib{
         std::reverse_iterator<T*> rend() noexcept {
             return std::reverse_iterator<T*>(begin());
         }
+    private:
+        T get3x3Determinant(std::size_t row, std::size_t col){
+            return (m_data[row][col] * ((m_data[row + 1][col + 1] * m_data[row + 2][col + 2]) - (m_data[row + 1][col + 2] * m_data[row + 2][col + 1])))
+                 - (m_data[row][col + 1] * ((m_data[row + 1][col] * m_data[row + 2][col + 2]) - (m_data[row + 1][col + 2] * m_data[row + 2][col])))
+                 + (m_data[row][col + 2] * ((m_data[row + 1][col] * m_data[row + 2][col + 1]) - (m_data[row + 1][col + 1] * m_data[row + 2][col])));
+        }
     };
 }
-
 
 #endif //MATLIB_MATRIX_HPP
