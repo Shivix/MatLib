24f249fa0680b0e62d773d28e85f2580829cdd1f
Shivix <52997365+Shivix@users.noreply.github.com>
2020-08-01 17:33:35 +0100

    added alias

--- python/MatLib/pyMatrix.hpp
+++ python/MatLib/pyMatrix.hpp
@@ -2,13 +2,16 @@
 #define MATLIB_PYMATRIX_CPP
 
 #include <pybind11/pybind11.h>
-#include "../../C++/MatLib/matrix.hpp"
+#include "../../cpp/MatLib/matrix.hpp"
 
 namespace py = pybind11;
+using matrix2x2 = MatLib::matrix<double, 2, 2>;
+using matrix3x3 = MatLib::matrix<double, 3, 3>;
 
 PYBIND11_MODULE(matrix, matrix){
-    py::class_<MatLib::matrix<double, 3, 3>>(matrix, "matrix")
-        .def(py::init<>());
+    py::class_<matrix3x3>(matrix, "matrix")
+        .def(py::init<>())
+        .def("empty", &matrix3x3::empty, "Returns true if the matrix has a size of 0");
 }
 
 #endif //MATLIB_PYMATRIX_CPP
